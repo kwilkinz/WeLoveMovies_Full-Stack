c0e0f73463fa72ccf82f1670c76bb4db7a816785
Keri Wilkins <keriannwilkins@gmail.com>
2022-11-17 14:09:36 -0600

    ERROR:

--- backend/src/errors/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(req, res, next) {
-    const { status = 500, message = "Something went wrong!" } = error;
-    res.status(status).json({ error: message })
-};
-
-module.exports = errorHandler;
--- backend/src/errors/notFound.js
+++
@@ -1,7 +0,0 @@
-function notFound(req, res, next) {
-    next({ 
-      status: 404, 
-      message: `Not found: ${req.originalUrl}`, });
-  }
-  
-  module.exports = notFound;
